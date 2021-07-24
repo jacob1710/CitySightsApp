d9e2d71337d1eddd112ee2ddc83aae4da216629b
Jacob Scase <jacobscase@hotmail.com>
2021-07-24 13:40:18 +0100

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Jacob Scase on 24/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
