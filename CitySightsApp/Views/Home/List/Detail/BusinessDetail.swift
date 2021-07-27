89103419bbbb36d9d1a99d6c1d0741c7c3505de9
Jacob Scase <jacobscase@hotmail.com>
2021-07-27 17:36:44 +0100

    Add growing and shrinking header image,

--- CitySightsApp/Views/Home/List/Detail/BusinessDetail.swift
+++ CitySightsApp/Views/Home/List/Detail/BusinessDetail.swift
@@ -9,111 +9,131 @@ import SwiftUI
 
 struct BusinessDetail: View {
     
+   
+    
     var business: Business
     
     var body: some View {
-        VStack(alignment: .leading){
-            
-            VStack(alignment: .leading, spacing: 0){
-                GeometryReader(){ geo in
-                    let uiImage = UIImage(data: business.imageData ?? Data())
-                    Image(uiImage: uiImage ?? UIImage())
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: geo.size.width, height: geo.size.height)
-                        .clipped()
+        ScrollView {
+            VStack(alignment: .leading){
+                
+                VStack(alignment: .leading, spacing: 0){
+                    GeometryReader(){ geo in
+                        let uiImage = UIImage(data: business.imageData ?? Data())
+                        
+                        //https://blckbirds.com/post/stretchy-header-and-parallax-scrolling-in-swiftui/
+                        
+                        if geo.frame(in: .global).minY <= 0 {
+                            Image(uiImage: uiImage ?? UIImage())
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: geo.size.width, height: geo.size.height)
+                                .offset(y: geo.frame(in: .global).minY/9)
+                                .clipped()
+                        }else{
+                            Image(uiImage: uiImage ?? UIImage())
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: geo.size.width, height: geo.size.height + geo.frame(in: .global).minY)
+                                .clipped()
+                                .offset(y: -geo.frame(in: .global).minY)
+                        }
+                        
+                    }
                     
-                }
-                .ignoresSafeArea(.all, edges: .top)
-                
-                ZStack(alignment: .leading){
-                    Rectangle()
-                        .frame(height: 36)
-                        .foregroundColor(business.isClosed! ? .gray:.blue)
-                    Text(business.isClosed! ? "Closed":"Open")
-                        .foregroundColor(.white)
-                        .bold()
-                        .padding(.leading)
+                    .frame(height: 250)
                     
-                }
-            }
-            Group{
-                //Name
-                Text(business.name!)
-                    .font(.largeTitle)
-                    .padding()
-                
-                
-                //address
-                if business.location?.displayAddress !=  nil{
-                    ForEach(business.location!.displayAddress!, id: \.self) { addressLine in
-                        Text(addressLine)
-                            .padding(.horizontal)
+                    
+                    ZStack(alignment: .leading){
+                        Rectangle()
+                            .frame(height: 36)
+                            .foregroundColor(business.isClosed! ? .gray:.blue)
+                        Text(business.isClosed! ? "Closed":"Open")
+                            .foregroundColor(.white)
+                            .bold()
+                            .padding(.leading)
+                        
                     }
                 }
                 
-                //rating
-                Image("regular_\(business.rating ?? 0)")
+                Group{
+                    //Name
+                    Text(business.name!)
+                        .font(.largeTitle)
+                        .padding()
+                    
+                    
+                    //address
+                    if business.location?.displayAddress !=  nil{
+                        ForEach(business.location!.displayAddress!, id: \.self) { addressLine in
+                            Text(addressLine)
+                                .padding(.horizontal)
+                        }
+                    }
+                    
+                    //rating
+                    Image("regular_\(business.rating ?? 0)")
+                        .padding()
+                    
+                    Divider()
+                    
+                    //phone
+                    HStack{
+                        Text("Phone")
+                            .bold()
+                        Text(business.displayPhone ?? "")
+                        Spacer()
+                        Link("Call", destination: URL(string: "tel:\(business.phone ?? "")")!)
+                    }
                     .padding()
-                
-                Divider()
-                
-                //phone
-                HStack{
-                    Text("Phone")
-                        .bold()
-                    Text(business.displayPhone ?? "")
-                    Spacer()
-                    Link("Call", destination: URL(string: "tel:\(business.phone ?? "")")!)
+                    
+                    Divider()
+                    
+                    //Reviews
+                    HStack{
+                        Text("Reviews")
+                            .bold()
+                        Text(String(business.reviewCount ?? 0))
+                        Spacer()
+                        Link("Read", destination: URL(string: business.url ?? "")!)
+                    }
+                    .padding()
+                    
+                    Divider()
+                    
+                    //Web
+                    HStack{
+                        Text("Website")
+                            .bold()
+                        Text(business.url ?? "")
+                            .lineLimit(1)
+                        Spacer()
+                        Link("Visit", destination: URL(string: business.url ?? "")!)
+                    }
+                    .padding()
+                    
+                    Divider()
                 }
+                
+                //Directions btn
+                Button(action: {
+                    //TODO: Show directions
+                    
+                }, label: {
+                    ZStack{
+                        Rectangle()
+                            .frame(height: 48)
+                            .foregroundColor(.blue)
+                            .cornerRadius(10)
+                        Text("Get Directions")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                })
                 .padding()
-                
-                Divider()
-                
-                //Reviews
-                HStack{
-                    Text("Reviews")
-                        .bold()
-                    Text(String(business.reviewCount ?? 0))
-                    Spacer()
-                    Link("Read", destination: URL(string: business.url ?? "")!)
-                }
-                .padding()
-                
-                Divider()
-                
-                //Web
-                HStack{
-                    Text("Website")
-                        .bold()
-                    Text(business.url ?? "")
-                        .lineLimit(1)
-                    Spacer()
-                    Link("Visit", destination: URL(string: business.url ?? "")!)
-                }
-                .padding()
-                
-                Divider()
             }
-            
-            //Directions btn
-            Button(action: {
-                //TODO: Show directions
-                
-            }, label: {
-                ZStack{
-                    Rectangle()
-                        .frame(height: 48)
-                        .foregroundColor(.blue)
-                        .cornerRadius(10)
-                    Text("Get Directions")
-                        .foregroundColor(.white)
-                        .bold()
-                }
-            })
-            .padding()
         }
+        .ignoresSafeArea(.all, edges: .top)
         
     }
 }
-
